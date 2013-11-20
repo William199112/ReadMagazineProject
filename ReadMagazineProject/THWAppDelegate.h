e56f4636d7e6642e6855fad812aefa432b3f7d28
krystalki <shangdejigou@tonghaowei.local>
2013-11-20 11:21:02 +0800

    Initial Commit

---
+++ ReadMagazineProject/THWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  THWAppDelegate.h
+//  ReadMagazineProject
+//
+//  Created by William on 13-11-20.
+//  Copyright (c) 2013年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
